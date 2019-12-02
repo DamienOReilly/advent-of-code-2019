b41ec50fc1e8f23f603fd2d5f5b2e00b0f544f79
Damien O'Reilly <damienreilly@gmail.com>
2019-12-02 14:24:54 +0000

    day02

---
+++ src/day02.rs
@@ -0,0 +1,58 @@
+static ADD: usize = 1;
+static MUL: usize = 2;
+static END: usize = 99;
+
+pub fn part_1(input: &str) -> Result<usize, String> {
+    let opcodes: Vec<usize> = input.split(",").map(|s| s.parse().unwrap()).collect();
+    return solve(opcodes, 12, 2);
+}
+
+pub fn part_2(input: &str) -> Result<usize, String> {
+    let opcodes: Vec<usize> = input.split(",").map(|s| s.parse().unwrap()).collect();
+    for n in 0..99 {
+        for v in 0..99 {
+            let copy = opcodes.clone();
+            let attempt = solve(copy, n, v);
+
+            match attempt {
+                Ok(res) if res == 19690720 => { return Ok(100 * n + v); }
+                _ => { continue; }
+            }
+        }
+    }
+
+    return Err("Cannot solve!".to_string());
+}
+
+fn solve(mut opcodes: Vec<usize>, noun: usize, verb: usize) -> Result<usize, String> {
+    opcodes[1] = noun;
+    opcodes[2] = verb;
+
+    let mut position = 0;
+    let opcode_len = opcodes.len();
+
+    while position < opcode_len {
+        match opcodes[position] {
+            x if x == ADD || x == MUL => {
+                let sum = if x == ADD {
+                    opcodes[opcodes[position + 1]] + opcodes[opcodes[position + 2]]
+                } else {
+                    opcodes[opcodes[position + 1]] * opcodes[opcodes[position + 2]]
+                };
+
+                let res_position = opcodes[position + 3];
+                opcodes[res_position] = sum;
+                position += 4;
+            }
+
+            x if x == END => {
+                break;
+            }
+
+            _ => { return Err(format!("Unknown opcode {} at position {}", opcodes[position], position));
+            }
+        }
+    }
+
+    return Ok(opcodes[0]);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 mod day01;
+mod day02;
 
 static DAY01_INPUT: &'static str = include_str!("day01.txt");
-
+static DAY02_INPUT: &'static str = include_str!("day02.txt");
 
 fn main() {
     let answer_1a = day01::part_1(DAY01_INPUT);
-    println!("Part 1 {:?}", answer_1a);
+    println!("Day 1 - Part 1 {:?}", answer_1a);
     let answer_1b = day01::part_2(DAY01_INPUT);
-    println!("Part 2 {:?}", answer_1b);
+    println!("Day 1 - Part 2 {:?}", answer_1b);
+
+    let answer_2a = day02::part_1(DAY02_INPUT);
+    println!("Day 2 - Part 1 {:?}", answer_2a);
+    let answer_2b = day02::part_2(DAY02_INPUT);
+    println!("Day 2 - Part 2 {:?}", answer_2b);
 }
