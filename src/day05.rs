e8b0ec1b1e07328aa71135e4ca561efe86ba29eb
Damien O'Reilly <damienreilly@gmail.com>
2019-12-05 20:39:23 +0000

    Completes Day05

--- src/day05.rs
+++ src/day05.rs
@@ -1,90 +1,129 @@
-use std::convert::TryFrom;
-
 static ADD: i64 = 1;
 static MUL: i64 = 2;
-static IN: i64 = 3;
+static IN: i64  = 3;
 static OUT: i64 = 4;
+static JNT: i64 = 5;
+static JNF: i64 = 6;
+static LT: i64  = 7;
+static EQ: i64  = 8;
 static END: i64 = 99;
 
-static INPUT_VALUE: i64 = 1;
-
-pub fn part_1(input: &str) {
-    let opcodes: Vec<i64> = input.split(",").map(|s| s.parse().unwrap()).collect();
-    solve(opcodes, INPUT_VALUE);
+pub fn part_1(input: &str) -> Result<i64, String> {
+    let mut opcodes: Vec<i64> = input.split(",").map(|s| s.parse().unwrap()).collect();
+    solve(&mut opcodes, 1)
 }
 
-fn solve(mut opcodes: Vec<i64>, input: i64) {
+pub fn part_2(input: &str) -> Result<i64, String> {
+    let mut opcodes: Vec<i64> = input.split(",").map(|s| s.parse().unwrap()).collect();
+    solve(&mut opcodes, 5)
+}
+
+fn get_parameters(immediate_mode: bool, position: usize, opcodes: Vec<i64>) -> i64 { 
+        if immediate_mode { 
+            opcodes[position]
+        } else {
+            opcodes[opcodes[position] as usize]
+        }
+}
+
+fn write_value(position: usize, value: i64, opcodes: &mut Vec<i64>) -> usize {
+    let res_position = opcodes[position] as usize;
+    opcodes[res_position] = value;
+    4
+}
+
+fn solve(opcodes: &mut Vec<i64>, input: i64) -> Result<i64, String> {
     let mut position = 0;
     let opcode_len = opcodes.len();
+    let mut diagnostic_codes = Vec::new();
 
     while position < opcode_len {
+        let first_param_immediate_mode  = opcodes[position] > 100  && opcodes[position] / 100  % 10 > 0;
+        let second_param_immediate_mode = opcodes[position] > 1000 && opcodes[position] / 1000 % 10 > 0;
         match opcodes[position] {
             op if op % 10 == ADD || op % 10 == MUL => {
-                let first_param_immediate_mode  = op > 100  && op / 100  % 10 > 0;
-                let second_param_immediate_mode = op > 1000 && op / 1000 % 10 > 0;
-                let x = { 
-                    if first_param_immediate_mode { 
-                        println!("First param at position {} with immediate mode with value [{}]", position + 1, opcodes[position + 1]);
-                        opcodes[position + 1]
-                    } else {
-                        println!("First param at position {} with position mode with offset [{}] and value [{}]", position + 1, opcodes[position + 1], opcodes[usize::try_from(opcodes[position + 2]).unwrap()]);
-                        opcodes[usize::try_from(opcodes[position + 1]).unwrap()]
-                    }
-                };
-
-                let y = { 
-                    if second_param_immediate_mode { 
-                        println!("Second param at position {} with immediate mode with value [{}]", position + 2, opcodes[position + 2]);
-                        opcodes[position + 2]
-                    } else {
-                        println!("Second param at position {} with position mode with offset [{}] and value [{}]", position + 2, opcodes[position + 2], opcodes[usize::try_from(opcodes[position + 2]).unwrap()]);
-                        opcodes[usize::try_from(opcodes[position + 2]).unwrap()]
-                    }
-                };
+                let x = get_parameters(first_param_immediate_mode, position + 1, opcodes.clone());
+                let y = get_parameters(second_param_immediate_mode, position + 2, opcodes.clone());
 
                 let res = if op % 10 == ADD {
-                    println!("adding");
                     x + y
                 } else {
-                    println!("muliplying");
                     x * y
                 };
-
-                let res_position = usize::try_from(opcodes[position + 3]).unwrap();
-                println!("Current position [{}] points to offset [{}] that has value [{}], saving [{}]..", position + 3, opcodes[position + 3], opcodes[res_position], res);
-                opcodes[res_position] = res;
-                position += 4;
+                position += write_value(position + 3, res, opcodes);
             }
 
             op if op == IN => {
-                let location = usize::try_from(opcodes[position + 1]).unwrap();
+                let location = opcodes[position + 1] as usize;
                 opcodes[location] = input;
                 position += 2;
             }
 
             op if op % 10 == OUT => {
-                let first_param_immediate_mode = op > 100 && op / 100 % 10 > 0;                
                 let output = { 
                     if first_param_immediate_mode { 
                         opcodes[position + 1]
                     } else {
-                        opcodes[usize::try_from(opcodes[position + 1]).unwrap()]
+                        opcodes[opcodes[position + 1] as usize]
                     }
                 };                
-                println!("OUTPUT!: {}", output);
+                diagnostic_codes.push(output);
                 position += 2;
             }
 
-            op if op == END => break,
-            
-            _ => {
-                opcodes.iter().for_each(|o| print!("{},", o));
-                println!("Unknown opcode {} at position {}.", opcodes[position], position);
-                break;
+            op if op % 10 == JNT => {
+                let x = get_parameters(first_param_immediate_mode, position + 1, opcodes.clone());
+                let y = get_parameters(second_param_immediate_mode, position + 2, opcodes.clone());
+
+                if x != 0 {
+                    position = y as usize;
+                } else {
+                    position += 3;
+                }
             }
 
-        }
+            op if op % 10 == JNF => {
+                let x = get_parameters(first_param_immediate_mode, position + 1, opcodes.clone());
+                let y = get_parameters(second_param_immediate_mode, position + 2, opcodes.clone());
 
+                if x == 0 {
+                    position = y as usize;
+                } else {
+                    position += 3;
+                }
+            }
+
+            op if op % 10 == LT => {
+                let x = get_parameters(first_param_immediate_mode, position + 1, opcodes.clone());
+                let y = get_parameters(second_param_immediate_mode, position + 2, opcodes.clone());
+
+                let res = if x < y {
+                    1
+                } else {
+                    0
+                };
+                position += write_value(position + 3, res, opcodes);
+            }
+
+            op if op % 10 == EQ => {
+                let x = get_parameters(first_param_immediate_mode, position + 1, opcodes.clone());
+                let y = get_parameters(second_param_immediate_mode, position + 2, opcodes.clone());
+
+                let res = if x == y {
+                    1
+                } else {
+                    0
+                };
+                position += write_value(position + 3, res, opcodes);
+            }
+
+            op if op == END => break,
+            _ => return Err(format!("Unknown opcode {} at position {}", opcodes[position], position))
+        }
     }
-    
+
+    match diagnostic_codes.last() {
+        Some(n) => Ok(*n),
+        None    => Err("Failed to solve puzzle!".into())        
+    }
 }
